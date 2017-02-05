4bad80b9f47c16a8dcc70b78b8206f085cf8dc00
mac <mac@mactekiMacBook-Pro.local>
2017-02-05 14:28:17 -0800

    project first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '575af961e39097fc109be2ac5583bb91444a86c0de1f8ae0b643c0e10e28777901c636ace54d6357ca673679610a7e4c13e912713ca518e5c5dd203b3bcedc5b'
